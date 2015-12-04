335783e477ab49a42851a29ee7b72b678f4d488f
MateuszMateo <k.mateusz120@gmail.com>
2015-12-04 16:29:17 +0100

    zamiana funkcji potęgowania "pow" na własną

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+    long long potega(int d,int f){
+        int g=d;
+        if(f==1)
+            return d;
+        else
+            for(int i=1;i<f;i++){
+                g=g*d;
+            }
+            return g;
+    }
+
 int main()
 {
     long long n,d;
@@ -19,18 +30,18 @@ int main()
     cout<<"Podaj dokladnosc testu:"<<endl;
     cin>>k;
     int s=1;
-    while(((n-1)%pow(2,s))>0){
+    while(((n-1)%potega(2,s))>0){
        s = s+1;
     }
     s=s-1;
-    d = n/pow(2,s);
+    d = n/potega(2,s);
     for(int i=0;i<k;i++){
         int a=rand()%n;
-        if(pow(a,d)%n==0)
+        if(potega(a,d)%n==0)
             wynik=0;
         else
             for(int r=0;r<s-1;s++){
-                if(pow(a,d*pow(2,r))==0)
+                if(potega(a,d*potega(2,r))==0)
                     wynik=1;
                 else
                     wynik=0;
@@ -39,7 +50,7 @@ int main()
     if(wynik==0)
         cout<<"liczba nie jest pierwsza"<<endl;
     if(wynik==1)
-        cout<<"liczba prawdopodobnie pierwsza"<<endl;
+        cout<<"liczba prawdopodobnie pierwsza :("<<endl;
 
     return 0;
 }
